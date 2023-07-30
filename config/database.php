2b73ae174f278f08bc91db2c756247b1eead1a5c
sw6thi <swathimano0610@gmail.com>
2023-07-30 19:01:12 +0800

    commit

--- config/database.php
+++ config/database.php
@@ -45,14 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL','https://laravel-mytodo-d6b4df3e4c84.herokuapp.com/
-            '),
-            'host' => env('DB_HOST', 'us-cdbr-east-06.cleardb.net'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'https://laravel-mytodo-d6b4df3e4c84.herokuapp.com/
-            '),
-            'password' => env('DB_PASSWORD', 'bc333545'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
